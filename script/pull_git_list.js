91dfd0bd3ec09f74709b9551ef0be25ee4bd468b
frankhan_i <frankhan_i@didiglobal.com>
2019-09-23 09:43:32 +0800

    commit auto 1569203012

--- script/pull_git_list.js
+++ script/pull_git_list.js
@@ -1,6 +1,7 @@
-function getList(func) {
+function extracted(func, url) {
     var request = new XMLHttpRequest();
-    request.open("GET", "./script/gitlist.json", true);
+    console.log(url);
+    request.open("GET", url, true);
     request.send();
     request.onreadystatechange = function () {
         if (request.readyState === 4 && request.status === 200) {
@@ -9,10 +10,14 @@ function getList(func) {
     };
 }
 
-function getListItemFunc(func) {
+function getList(func, url) {
+    extracted(func, url);
+}
+
+function getListItemFunc(func, url) {
     getList(function (k) {
         for (let i = 0; i < k.length; i++) {
             func(k[i])
         }
-    })
+    }, url)
 }
