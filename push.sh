df577a5667eb8f67eea7bae7f346a6f68b7bda5b
frankhan_i <frankhan_i@didiglobal.com>
2019-10-02 10:50:05 +0800

    commit auto 1569984604

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
 git add *
-echo "[UPDATE] auto commit update `date +%s`" >> commit
-git commit -m "commit auto `date +%s`"
+echo "[UPDATE] auto commit update $(date +%s)" >> commit
+git commit -m "commit auto $(date +%s)"
 git push
