8f087970b355eb28f813d59f22c456b62f0ff0f7
frank <lnpj.office@icloud.com>
2019-12-16 15:19:37 +0800

    commit auto 1576480777

---
+++ initbook.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+dir="./library-book"
+library="data/library"
+
+
+function findByDirectory() {
+    now="$(ls $1)"
+    for name in ${now}
+    do
+        next="${1}/$name"
+        if [[ -d ${next} ]]; then
+            findByDirectory ${next}
+        else
+            echo ${next} >> "$library"
+        fi
+    done
+}
+echo "SYSTEM INIT DATA TIME:$(date)" > "$library"
+findByDirectory "${dir}"
