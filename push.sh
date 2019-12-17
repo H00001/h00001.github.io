c0bb7681885b1dfdf06e8023d04944bf9e06a7b0
frank <lnpj.office@icloud.com>
2019-12-17 18:11:49 +0800

    commit auto 1576577509

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git pull
+git pull origin master  --allow-unrelated-histories	
 echo "[UPDATE] auto commit update $(date +%s)" >> commit
 git add *
 git commit -m "commit auto $(date +%s)"
-git push
+git push origin master
