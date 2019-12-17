5455dd88d031be488ab4f705efdf833f15c099c7
frank <lnpj.office@icloud.com>
2019-12-17 15:25:25 +0800

    commit auto 1576567525

--- script/pull_git_list.js
+++ script/pull_git_list.js
@@ -1,10 +1,18 @@
-function extracted(func, url) {
+function extracted(func, url, isToObj, error) {
     var request = new XMLHttpRequest();
     request.open("GET", url, true);
     request.send();
     request.onreadystatechange = function () {
         if (request.readyState === 4 && request.status === 200) {
-            func(eval("(" + request.responseText + ")"));
+            try {
+                if (isToObj === true || isToObj === undefined) {
+                    func(eval("(" + request.responseText + ")"));
+                } else {
+                    func(request.responseText)
+                }
+            } catch (e) {
+                error(e)
+            }
         }
     };
 }
@@ -16,7 +24,8 @@ function getList(func, url) {
 function getListItemFunc(func, url) {
     getList(function (k) {
         for (let i = 0; i < k.length; i++) {
-            func(k[i],i)
+            func(k[i], i)
         }
     }, url)
 }
+
