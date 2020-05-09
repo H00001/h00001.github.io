b3952f2ff2f2c3ab5459ec010be76c97d00c5fed
frankhan <system-noreply@gunplan.top>
2020-05-09 18:47:03 +0800

    push

--- _site/push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull origin master  --allow-unrelated-histories	
-echo "[UPDATE] auto commit update $(date +%s)" >> commit
-./initbook.sh
-git add *
-git commit -m "commit auto $(date +%s)"
-git push origin master
-
